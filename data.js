0fa10e51ac8162ec3ceb18361f5e5ff7c49e3387
Helondale <113353417+Helondale@users.noreply.github.com>
2022-12-28 21:16:13 +0100

    Update data.js
    Added methods and functions

--- data.js
+++ data.js
@@ -1,9 +1,67 @@
 // YOUR CODE COMES HERE
+function stringRepeat(number, str){
+    return(str.repeat(number))
 
 
+};
+console.log(stringRepeat(2, "HawaiiPizza"));
 
+function noSpace(str){
+    return(str.replace(/\s+/g,''))
 
+};
+console.log(noSpace(" Ha wa iiP z za "));
 
+function numberToString(number){
+    return(number.toString());
+    
+
+};
+console.log(numberToString(33));
+
+function booleanToString(boolean){
+return(boolean.toString(boolean))
+
+};
+console.log(booleanToString(true));
+
+function abbrevName(string){
+    return string
+        .replace(/\b(\w)\w+/g, '$1.')
+        .replace(/\s/g, '')
+        .replace(/\.$/, '')
+        .toUpperCase();
+
+}
+console.log(abbrevName("Hawaii Pizza"))
+
+function nameLength(string){
+   let words = string.split(' ')
+   let wordsLength = []
+   words.forEach((cica) => {
+    wordsLength.push(cica + " = " +cica.length)
+
+   });
+   return wordsLength
+};
+console.log(nameLength("Hawaii Pizza"));
+
+function removeOrders(string){   
+return string.substring(1).slice(0,-1)
+
+};
+console.log(removeOrders('1,2,3,4,5'));
+
+function foodMenu(array){
+let result = []
+for (let i=1; i<=array.length; i++){
+result.push(`${i}. ${array[i-1]}`)
+//result.push(i+". " + array[i-1])
+}
+return result
+
+};
+console.log(foodMenu(["Hawaii Pizza", "Gyros Pizza"]));
 
 
 
