ce4b9d5c28f6037ba4018dfa047ee3049eeb3ca1
Helondale <hatvaniadrienn99@gmail.com>
2023-05-23 12:09:43 +0200

    Few changes for cleaner code

--- data.js
+++ data.js
@@ -1,27 +1,20 @@
-// YOUR CODE COMES HERE
 function stringRepeat(number, str){
     return(str.repeat(number))
-
-
 };
 console.log(stringRepeat(2, "HawaiiPizza"));
 
 function noSpace(str){
     return(str.replace(/\s+/g,''))
-
 };
 console.log(noSpace(" Ha wa iiP z za "));
 
 function numberToString(number){
     return(number.toString());
-    
-
 };
 console.log(numberToString(33));
 
 function booleanToString(boolean){
 return(boolean.toString(boolean))
-
 };
 console.log(booleanToString(true));
 
@@ -31,16 +24,14 @@ function abbrevName(string){
         .replace(/\s/g, '')
         .replace(/\.$/, '')
         .toUpperCase();
-
-}
+};
 console.log(abbrevName("Hawaii Pizza"))
 
 function nameLength(string){
    let words = string.split(' ')
    let wordsLength = []
-   words.forEach((cica) => {
-    wordsLength.push(cica + " = " +cica.length)
-
+   words.forEach((element) => {
+    wordsLength.push(element + " = " + element.length)
    });
    return wordsLength
 };
@@ -56,50 +47,7 @@ function foodMenu(array){
 let result = []
 for (let i=1; i<=array.length; i++){
 result.push(`${i}. ${array[i-1]}`)
-//result.push(i+". " + array[i-1])
 }
 return result
-
 };
 console.log(foodMenu(["Hawaii Pizza", "Gyros Pizza"]));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// DO NOT MODIFY THE CODE BELOW THIS LINE
-let toExport;
-
-try {
-    toExport = [
-        {name: "stringRepeat", content: stringRepeat, type: "function"},
-        {name: "noSpace", content: noSpace, type: "function"},
-        {name: "numberToString", content: numberToString, type: "function"},
-        {name: "booleanToString", content: booleanToString, type: "function"},
-        {name: "abbrevName", content: abbrevName, type: "function"},
-        {name: "nameLength", content: nameLength, type: "function"},
-        {name: "removeOrders", content: removeOrders, type: "function"},
-        {name: "foodMenu", content: foodMenu, type: "function"}
-    ]
-} catch (error) {
-    toExport = {error: error.message}
-}
-
-export {toExport};
